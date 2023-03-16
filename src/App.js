337ef70cde89b7b44eedab0004936a15eb74c763
CherokeeX <serefb80@gmail.com>
2023-03-16 16:36:39 +0300

    useStates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  //  const [todos,setTodos] = useState ([])
+  //  const [todo, setTodo]= useState()
+  //  const submitHandle =(e) => {
+  // e.preventDefault ()
+  // setTodos([...todos,todo])
+  // setTodo('')
+
+  //  }
+
+  // return(
+
+  // <>
+  // <h2>useReduse</h2>
+
+  // <form onSubmit={submitHandle}>
+  //   <input type="text"  value={todo} onChange= { e => setTodo(e.target.value)}/>
+  //   <button type="submit">EKLE</button>
+  // </form>
+
+  // <ul>
+  //   {todos.map((todo,index)=>{return(<li key={index}>{todo}</li>)})}
+  // </ul>
+  //)
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const submitHandle = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> useReduce finnal coding</h1>
+
+      <form onSubmit={submitHandle}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button disabled={!todo} type="submit">
+          Add TODO
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((todo, index) => {
+          return <li key={index}> {todo}</li>;
+        })}
+      </ul>
+    </>
   );
 }
 
